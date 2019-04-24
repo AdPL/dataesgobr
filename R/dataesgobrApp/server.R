52e8c5852d0e76e1a85584e45f219079a10a58fa
Adrian Perez <adrianpelopez@gmail.com>
2019-04-24 18:08:39 +0200

    Add 'load by id' view

--- R/dataesgobrApp/server.R
+++ R/dataesgobrApp/server.R
@@ -10,4 +10,19 @@ server <- function(input, output) {
       data
     }))
   })
+
+  observeEvent(input$submitId, {
+    data <- search_by_id(input$url)
+    output$datasetTitle <- renderText(data$title)
+    output$datasetUrl <- renderText(data$url)
+    output$datasetPublisher <- renderText(data$publisher)
+    output$datasetDescription <- renderText(data$description[[1]])
+    formats <- do.call(rbind,
+                       Map(data.frame,
+                           Format = names(data$formats),
+                           Url = data$formats,
+                           Information = data$formats_info))
+    output$datasetFormats <- renderTable(formats)
+    output$datasetIssued <- renderText(data$issued)
+  })
 }
--- R/dataesgobrApp/ui.R
+++ R/dataesgobrApp/ui.R
@@ -1,18 +1,46 @@
 library(shiny)
 
-ui <- navbarPage(
-  "DataesgobR",
-  tabPanel("Search"),
+ui <- fluidPage(
+  navbarPage("DataesgobR!",
+  tabPanel("Search",
   sidebarLayout(
     sidebarPanel(
       textInput(inputId = "title", label = "Title"),
-      actionButton("submit", "Search")
-    ),
-    mainPanel(
-      fluidRow(
-        column(12,
-          DT::dataTableOutput('datasetsTable'))
+                actionButton("submit", "Search")
+      ),
+      mainPanel(
+        fluidRow(
+          column(12,
+                 DT::dataTableOutput('datasetsTable')
+          )
+        )
       )
     )
+  ),
+  tabPanel("Load by ID",
+   fluidRow(
+     column(3,
+      wellPanel(
+        textInput(inputId = "url", label = "ID"),
+        actionButton("submitId", "Load")
+      )
+     ),
+     column(9,
+      wellPanel(
+        h1(textOutput("datasetTitle")),
+        h5(textOutput("datasetUrl")),
+        strong("Publisher:"),
+        span(textOutput("datasetPublisher")),
+        hr(),
+        h3("Description"),
+        textOutput("datasetDescription"),
+        h3("Distribution"),
+        tableOutput("datasetFormats"),
+        h3("Additional info"),
+        textOutput("datasetIssued")
+      )
+     )
+    )
+   )
   )
 )
