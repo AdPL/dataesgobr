fc45a11cace2ab8121fdee71670daf7acdbd0ca0
Adrian Perez <adrianpelopez@gmail.com>
2019-05-07 20:10:09 +0200

    Update ShinyApp Redesigned app

--- R/dataesgobrApp/server.R
+++ R/dataesgobrApp/server.R
@@ -2,27 +2,24 @@ library(dataesgobr)
 library(dplyr)
 
 server <- function(input, output, session) {
+  generateButton <- function(FUN, len, id, ...) {
+    buttons <- character(len)
+    for (i in seq_len(len)) {
+      buttons[i] <- as.character(FUN(paste0(id, i), ...))
+    }
+    buttons
+  }
+
   observeEvent(input$datasetsTable_rows_selected, {
     data <- datasets_downloaded
 
     row <- input$datasetsTable_rows_selected
     url <- as.character(dataesgobr:::get_id(data[row,]$Url))
-    dataSelected <- search_by_id(url)
+    data_preload <<- dataSelected <- search_by_id(url)
 
-    if (is.null(dataSelected$formats_info)) {
-      dataSelected$formats_info <- "No info"
-    }
 
-    output$titleSelected <- renderText(dataSelected$title)
-    output$formatsSelected <- renderDataTable({
-      formats <- do.call(rbind,
-                         Map(data.frame,
-                             Format = names(dataSelected$formats),
-                             Url = paste0("<a href='", dataSelected$formats,
-                                          "' target='_blank'>Descargar</a>"),
-                             Information = dataSelected$formats_info))
-      return(formats)
-    }, escape = FALSE)
+
+    addClass("formatsSelected", "table-responsive")
     return(data)
   })
 
@@ -33,53 +30,52 @@ server <- function(input, output, session) {
       output$datasetsTable <- DT::renderDT({})
     } else {
       output$searchState <- renderText("")
-      output$datasetsTable <- DT::renderDT({
-
+      output$datasetsTable <- DT::renderDataTable({
         data <- do.call(rbind,
                         Map(data.frame,
-                            Title = datasets$title,
-                            Description = datasets$description,
+                            ID = rownames(datasets),
+                            Title = as.character(datasets$title),
+                            Description = as.character(datasets$description),
                             About = paste0("<a href='", datasets$`_about`,
-                                           "' target='_blank'>Open</a>"),
+                                           "' target='_blank'>Open in datos.gob.es</a>"),
+                            Actions = generateButton(actionButton,
+                                                     nrow(datasets),
+                                                     'button_',
+                                                     label = "Load dataset",
+                                                     onclick = 'Shiny.onInputChange(\"select_button\", this.id)'),
                             Url = datasets$`_about`))
         datasets_downloaded <<- data
-        return(data[1:4])
+        return(data[2:5])
       }, escape = FALSE, selection = "single")
+      addClass("datasetsTable", "table-responsive")
     }
   })
 
-  observeEvent(input$submitId, {
-    data_preload <<- data <- search_by_id(input$url)
-    output$datasetTitle <- renderText(data$title)
-    output$datasetUrl <- renderText(data$url)
-    output$datasetPublisher <- renderText(data$publisher)
-    output$datasetDescription <- renderText(data$description[[1]])
+  observeEvent(input$select_button, {
+    datasetSelected <- as.numeric(strsplit(input$select_button, "_")[[1]][2])
+    data_preload <<- search_by_id(dataesgobr:::get_id(datasets_downloaded[datasetSelected,]$Url))
 
-    output$datasetFormats <- renderDataTable({
-      formats <- do.call(rbind,
-                         Map(data.frame,
-                             Format = names(data$formats),
-                             Url = paste0("<a href='", data$formats, "'>Descargar</a>"),
-                             Information = data$formats_info))
-      return(formats)
-    }, escape = FALSE)
-    output$datasetIssued <- renderText(data$issued)
-  })
+    showNotification(paste("Dataset loaded"), type = "message", duration = 4)
 
-  observeEvent(input$sendToWork, {
-    output$datasetLoadTitle <- renderText(data_preload$title)
-    output$datasetLoadFormats <- renderDataTable({
+    output$datasetTitleSelected <- renderText(data_preload$title)
+    output$datasetDescriptionSelected <- renderText(data_preload$description)
+    output$datasetPublisherSelected <- renderText(paste("Publisher: ", data_preload$publisher))
+
+    if (is.null(data_preload$formats_info)) {
+      data_preload$formats_info <- "No info"
+    }
+
+    output$datasetFormatsSelected <- DT::renderDataTable({
       formats <- do.call(rbind,
                          Map(data.frame,
                              Format = names(data_preload$formats),
-                             Url = paste0("<a href='", data_preload$formats, "'>Load data</a>"),
+                             Url = paste0("<a href='", data_preload$formats,
+                                          "' target='_blank'>Descargar</a>"),
                              Information = data_preload$formats_info))
       return(formats)
     }, escape = FALSE)
-  })
+    addClass("datasetFormatsSelected", "table-responsive")
+    updateTabsetPanel(session, "tabs", "Work")
 
-  output$loadCSV <- downloadHandler(
-    content <<- load_data(data_preload),
-    output$dataTable <- renderDataTable(content)
-  )
+  })
 }
--- R/dataesgobrApp/ui.R
+++ R/dataesgobrApp/ui.R
@@ -1,70 +1,47 @@
 library(shiny)
+library(shinythemes)
+library(shinyjs)
 
-ui <- fluidPage(
-  navbarPage("DataesgobR!",
-  tabPanel("Search",
-  sidebarLayout(
-    sidebarPanel(
-      textInput(inputId = "title", label = "Title"),
-                actionButton("submit", "Search"),
-                hr(),
-                h4(textOutput("titleSelected")),
-                dataTableOutput("formatsSelected"),
-                textOutput("error")
-      ),
-      mainPanel(
-        fluidRow(
-          column(12,
-                 wellPanel(
-                  textOutput("searchState"),
-                  DT::dataTableOutput("datasetsTable")
-                 )
-          )
-        )
-      )
-    )
-  ),
-  tabPanel("Load by ID",
-   fluidRow(
-     column(3,
-      wellPanel(
-        textInput(inputId = "url", label = "ID"),
-        actionButton("submitId", "Load")
-      )
-     ),
-     column(9,
-      wellPanel(
-        h1(textOutput("datasetTitle")),
-        actionButton("sendToWork", "Load in work view"),
-        h5(textOutput("datasetUrl")),
-        strong("Publisher:"),
-        span(textOutput("datasetPublisher")),
-        hr(),
-        h3("Description"),
-        textOutput("datasetDescription"),
-        h3("Distribution"),
-        dataTableOutput("datasetFormats"),
-        h3("Additional info"),
-        textOutput("datasetIssued")
-      )
-     )
-    )
-   ),
-  tabPanel("Work",
-     fluidRow(
-       column(3,
-              wellPanel(
-                h1(textOutput("datasetLoadTitle")),
-                dataTableOutput("datasetLoadFormats")
-              )
-       ),
-       column(9,
-              wellPanel(
-                downloadLink("loadCSV", "Load CSV"),
-                dataTableOutput("dataTable")
-              )
-       )
-     )
-    )
+ui <- bootstrapPage(theme = shinytheme("paper"),
+  useShinyjs(),
+  navbarPage("DataesgobR!", id = "tabs",
+             tabPanel("Main",
+                      wellPanel(
+                          tabsetPanel(
+                            tabPanel("Search by title",
+                                  textInput(inputId = "title", label = "",
+                                            placeholder = "Title"),
+                                  actionButton("submit", "Search"),
+                                  textOutput("error"),
+                                  textOutput("selected")
+                           )
+                         )
+                      ),
+                      fluidRow(
+                        column(12,
+                               wellPanel(
+                                 textOutput("searchState"),
+                                 DT::dataTableOutput("datasetsTable")
+                               )
+                        )
+                      )
+             ),
+             tabPanel("Work",
+                      fluidRow(
+                        column(6,
+                          wellPanel(
+                            h5(textOutput("datasetTitleSelected")),
+                            p(textOutput("datasetDescriptionSelected")),
+                            p(textOutput("datasetPublisherSelected"))
+                          )
+                        ),
+                        column(6,
+                           wellPanel(
+                            h5("Available Formats"),
+                            DT::dataTableOutput("datasetFormatsSelected")
+                           )
+                        )
+                      )
+            )
   )
 )
