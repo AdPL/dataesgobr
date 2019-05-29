4b85d5f4c1ac761883787efc0efc07ecf8042670
Adrian Perez <adrianpelopez@gmail.com>
2019-05-29 17:30:29 +0200

    Fix id parse

--- tests/testthat/test-make-url.R
+++ tests/testthat/test-make-url.R
@@ -8,4 +8,5 @@ test_that("Make url works", {
   expect_equal(make_url("theme", "sector-publico"), "https://datos.gob.es/apidata/catalog/dataset/theme/sector-publico")
   expect_equal(make_url("format", "json"), "https://datos.gob.es/apidata/catalog/dataset/format/json")
   expect_equal(make_url("keyword", "turismo"), "https://datos.gob.es/apidata/catalog/dataset/keyword/turismo")
+  expect_equal(make_url("id", "ea0010587-correspondencias-cruzadas-de-las-superficies-de-regadio-entre-las-comunidades-autonomas-y-las-demarcaciones-hidrograficas-ano-2011-comunidades-y-ciudades-autonomas-estadisticas-sobre-el-uso-del-agua-identificador-api-t26-p067-p05-l0-01001-px1"), "https://datos.gob.es/apidata/catalog/dataset/ea0010587-correspondencias-cruzadas-de-las-superficies-de-regadio-entre-las-comunidades-autonomas-y-las-demarcaciones-hidrograficas-ano-2011-comunidades-y-ciudades-autonomas-estadisticas-sobre-el-uso-del-agua-identificador-api-t26-p067-p05-l0-01001-px1")
 })
