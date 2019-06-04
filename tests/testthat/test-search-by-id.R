cbc58cd9afdc552eadc2d2704e3d7c31e696dcae
Adrian Perez <adrianpelopez@gmail.com>
2019-06-04 14:50:44 +0200

    Update test-search-by-id test

--- tests/testthat/test-search-by-id.R
+++ tests/testthat/test-search-by-id.R
@@ -1,16 +1,5 @@
 context("test-search-by-id")
 
-test_that("Search by id works", {
-  expect_is(search_by_id("https://datos.gob.es/es/catalogo/l01280148-contratos-mayores-4-trimestre-20161"),
-              "dataesgobr")
-  expect_is(search_by_id("l01280148-contratos-mayores-4-trimestre-20161"),
-              "dataesgobr")
-})
-
-test_that("Warning if dataset not found and return NULL", {
-  expect_null(search_by_id("l01280148-not-found"))
-})
-
 test_that("ID is not a character", {
   expect_error(search_by_id(123), "*is not TRUE")
   expect_error(search_by_id(list()), "*is not TRUE")
